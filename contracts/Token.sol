35688cbbdac75f0a49ba48a8b93554618904deb2
Max Dayton <daytonmaximus@gmail.com>
2022-03-05 20:16:46 -0700

    moved token contract from old repo

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,73 +1,40 @@
-//SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.0 <0.9.0;
+//SPDX-License-Identifier: MIT
 
-// Solidity files have to start with this pragma.
-// It will be used by the Solidity compiler to validate its version.
-pragma solidity ^0.7.0;
-
-// We import this library to be able to use console.log
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
+contract AuthToken is ERC721URIStorage, AccessControl, Ownable {
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+  address contractAddress;
 
-// This is the main building block for smart contracts.
-contract Token {
-    // Some string type variables to identify the token.
-    string public name = "My Hardhat Token";
-    string public symbol = "MHT";
+  bytes32 public constant VERIFIER_ROLE = keccak256("VERIFIER_ROLE"); 
 
-    // The fixed amount of tokens stored in an unsigned integer type variable.
-    uint256 public totalSupply = 1000000;
+  constructor() ERC721("HealerToken", "HEAL") {
+    // Give deployer account all permissions
+    _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+  }
 
-    // An address type variable is used to store ethereum accounts.
-    address public owner;
+  // Overide supportsInterface
+  function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, AccessControl) returns (bool) {
+    return super.supportsInterface(interfaceId);
+  }
 
-    // A mapping is a key/value map. Here we store each account balance.
-    mapping(address => uint256) balances;
+  // Mint Token
+  function createToken(address to, string memory tokenURI) public onlyRole(VERIFIER_ROLE) returns (uint256) {
+    require(hasRole(VERIFIER_ROLE, msg.sender));
+    _tokenIds.increment();
+    uint256 newItemId = _tokenIds.current();
 
-    /**
-     * Contract initialization.
-     *
-     * The `constructor` is executed only once when the contract is created.
-     * The `public` modifier makes a function callable from outside the contract.
-     */
-    constructor() {
-        // The totalSupply is assigned to transaction sender, which is the account
-        // that is deploying the contract.
-        balances[msg.sender] = totalSupply;
-        owner = msg.sender;
-    }
-
-    /**
-     * A function to transfer tokens.
-     *
-     * The `external` modifier makes a function *only* callable from outside
-     * the contract.
-     */
-    function transfer(address to, uint256 amount) external {
-        // Check if the transaction sender has enough tokens.
-        // If `require`'s first argument evaluates to `false` then the
-        // transaction will revert.
-        require(balances[msg.sender] >= amount, "Not enough tokens");
-
-        // We can print messages and values using console.log
-        console.log(
-            "Transferring from %s to %s %s tokens",
-            msg.sender,
-            to,
-            amount
-        );
-
-        // Transfer the amount.
-        balances[msg.sender] -= amount;
-        balances[to] += amount;
-    }
-
-    /**
-     * Read only function to retrieve the token balance of a given account.
-     *
-     * The `view` modifier indicates that it doesn't modify the contract's
-     * state, which allows us to call it without executing a transaction.
-     */
-    function balanceOf(address account) external view returns (uint256) {
-        return balances[account];
-    }
+    _safeMint(to, newItemId);
+    _setTokenURI(newItemId, tokenURI);
+    return newItemId;
+  }
+  
+  // TODO Add functions for issuing and permissions of tokens
 }
