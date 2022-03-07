99bd62f8a430129ade04af0add94ff1460fa9cff
Max Dayton <daytonmaximus@gmail.com>
2022-03-06 18:56:49 -0700

    updated documents contract to check for healer role

--- contracts/Documents.sol
+++ contracts/Documents.sol
@@ -4,16 +4,24 @@ pragma solidity ^0.8.0;
 import "./RoleControl.sol";
 // import "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
 
-contract Documents {
+contract Documents is RoleControl {
     mapping (address => string[]) public document_keys;
 
     // Set a new document key
-    function update(string memory _key) public {
-        document_keys[msg.sender].push(_key);
+    function update(string memory _key, address _patient) public {
+        if (isHealer(msg.sender)) {
+            document_keys[_patient].push(_key);
+        } else {
+            document_keys[msg.sender].push(_key);
+        }
     }
 
     // Get document keys by address
-    function get() view public returns(string[] memory) {
-        return document_keys[msg.sender];
+    function get(address _patient) view public returns(string[] memory) {
+        if (isHealer(msg.sender)) {
+            return document_keys[_patient];
+        } else {
+            return document_keys[msg.sender];
+        }
     }
 }
